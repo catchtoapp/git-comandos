c47a0735b03cc6dca52182b3e61d421f3c3c7b69
jmelendez <jhomeva@gmail.com>
2025-02-06 01:53:00 -0500

    update branch feature 0001

--- AplicacionConsola/Program.cs
+++ AplicacionConsola/Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ahora cambiamos de mensaje en develop!");
-Console.WriteLine("Nuevo mensaje en develop!");
-Console.WriteLine("otro mensaje en develop!");
+Console.WriteLine("Ahora cambiamos de mensaje en feature 0001!");
