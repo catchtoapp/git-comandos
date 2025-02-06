6fd8e1f2c2c25ec8d62060bcdea76fa010540e0a
jmelendez <jhomeva@gmail.com>
2025-02-06 01:06:45 -0500

    update Program.cs develop 3

--- AplicacionConsola/Program.cs
+++ AplicacionConsola/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Ahora cambiamos de mensaje en develop!");
 Console.WriteLine("Nuevo mensaje en develop!");
+Console.WriteLine("otro mensaje en develop!");
